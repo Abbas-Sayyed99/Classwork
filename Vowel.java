759becb07ee264dc7daef0ac212bdbae9acd284d
Abbas-Sayyed99 <106953462+Abbas-Sayyed99@users.noreply.github.com>
2022-06-23 13:50:38 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Vowel
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("\n\t\t\tVowel Or Consonant");
+		System.out.println("\t\t\t======================");
+
+		System.out.print("\n\t\t\tEnter Character : ");
+			char V = sc.next().charAt(0);
+		System.out.println("\n\t\t\t=================================");
+		
+		switch(V)
+		{
+			case 'a':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+			case 'A':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+
+			case 'e':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+
+			case 'E':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+ 			case 'i':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+			
+			case 'I':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+   			case 'o':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+                case 'O':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+
+                case 'u':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}    
+			
+
+                case 'U':
+			{
+				System.out.println("\t\t\tIt is vowel");
+				break;
+			}
+         		default:
+				System.out.println("\n\t\t\tIt is Consonant.");
+		}
+	}
+}
